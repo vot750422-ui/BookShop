1eb6d9cf131ad03f26fe9fe6c266942105875905
チョン <vot750422@gmail.com>
2026-03-21 11:02:35 +0700

    commit ngày 21/3

--- Config.php
+++ Config.php
@@ -1,9 +1,9 @@
 <?php
 $host     = "localhost";
 $database = "sakurabo1_db";
-$uid      = "admin";
-$pwd      = "Trong@6419423";        // WAMP mặc định để trống
-
+$uid      = "sakurabo1_admin";
+$pwd      = "Trong@6419423";       
+$charset = 'utf8mb4';
 try {
     $conn = new PDO(
         "mysql:host=$host;dbname=$database;charset=utf8",
